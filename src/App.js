8fe31f80e50734c0237a0228f5dd8f85ad1dd734
Emmanuelle  Dennemont <emmanuelledennemont@macbook-pro-de-emmanuelle.home>
2023-01-16 22:42:46 +0100

    navbar menu burger

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Header from './components/Header/Header'
 function App() {
   return (
     <>
-<h1>Hello world</h1>
+
 <Header/>
     </>
   );
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,32 @@
-import React from 'react'
+import logo from "../../assets/images/logo.svg";
+import burger from "../../assets/images/icon-hamburger.svg";
+import {useState} from "react";
 
 const Header = () => {
-  return ( 
+  const [showMenu, setShowMenu] = useState(false);
+  return (
     <>
-    <div className="p">Header</div>
+        <nav className="navBar container">
+          <div className="brand">
+            <div className="logo">
+              <img src={logo} alt="logo compagny name" />
+            </div>
+            <div className="menu__toggle">
+              <img src={burger} alt="" />
+            </div>
+          </div>
+          <div className="navBar__menu">
+            <ul className="navBar__list">
+              <li className="navBar__items">Home</li>
+              <li className="navBar__items">About</li>
+              <li className="navBar__items">Contact</li>
+              <li className="navBar__items">Blog</li>
+              <li className="navBar__items">Careers</li>
+            </ul>
+          </div>
+        </nav>
     </>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
